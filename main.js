fde3a7cf3ff2028a962a5a0df4880dce14527040
num <num>
2023-12-06 20:25:18 -0300

    adiciona o index, estoque.js e main.js com form

---
+++ main.js
@@ -0,0 +1,10 @@
+//main.js
+
+leFormulario();
+ 
+function leFormulario() {
+    const fruta = document.entrada.fruta.value;
+    const quantidade = document.entrada.fruta.value;
+
+    console.log(fruta, quantidade);
+}
